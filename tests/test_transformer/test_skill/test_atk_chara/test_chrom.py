8ab016abd4116a616ce05e7492bf281f46f3f601
RaenonX <raenonx0710@gmail.com>
2020-12-14 14:10:43 -0600

    OTH - Chrom notes
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- tests/test_transformer/test_skill/test_atk_chara/test_chrom.py
+++ tests/test_transformer/test_skill/test_atk_chara/test_chrom.py
@@ -1,1 +1,5 @@
 # TEST: TBA - Chrom (10150105): better representation
+#   s1_base / s2_base / 1 x S1 & 1 SP
+#   s2_enhanced_by_s1 / 2 x S1 & 1 SP
+#   s2_enhanced_by_s1 / s2_enhanced_by_ab918 / 3 x S1 & 1 SP
+#   s2_enhanced_by_ab914 / s2_enhanced_by_ab939 / 3 x S1 & 3 SP
