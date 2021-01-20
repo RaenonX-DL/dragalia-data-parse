c2fb68463ad9708d76c1d2bbfeb72a854a8d2ed8
RaenonX <raenonx0710@gmail.com>
2021-01-20 08:27:02 -0600

    ADD - Gala Mym S2 test (before and after shapeshifting)
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- tests/test_transformer/test_skill/test_atk_chara/test_mym_gala.py
+++ tests/test_transformer/test_skill/test_atk_chara/test_mym_gala.py
@@ -1,7 +1,10 @@
 from itertools import zip_longest
 
+import pytest
+
 from dlparse.enums import SkillCancelAction
 from dlparse.transformer import SkillTransformer
+from tests.utils import approx_matrix
 
 
 def test_cancel_s2_data(transformer_skill: SkillTransformer):
@@ -20,3 +23,47 @@ def test_cancel_s2_data(transformer_skill: SkillTransformer):
     for expected_lv, actual_lv in zip_longest(main_expected, main_actual):
         diff = expected_lv.symmetric_difference(actual_lv)
         assert len(diff) == 0, diff
+
+
+def test_s2(transformer_skill: SkillTransformer):
+    # Gala Mym S2
+    # https://dragalialost.gamepedia.com/Gala_Mym
+    skill_data_base = transformer_skill.transform_attacking(105501012)
+
+    skill_data = skill_data_base.with_conditions()
+
+    assert skill_data.hit_count == [4, 4]
+    assert skill_data.hit_count_at_max == 4
+    assert skill_data.total_mod == pytest.approx([
+        4.675 * 4,
+        5.175 * 4,
+    ])
+    assert skill_data.total_mod_at_max == pytest.approx(5.175 * 4)
+    assert skill_data.mods == approx_matrix([
+        [4.675] * 4,
+        [5.175] * 4,
+    ])
+    assert skill_data.mods_at_max == pytest.approx([5.175] * 4)
+    assert skill_data.max_level == 2
+
+
+def test_s2_shapeshifted(transformer_skill: SkillTransformer):
+    # Gala Mym S2
+    # https://dragalialost.gamepedia.com/Gala_Mym
+    skill_data_base = transformer_skill.transform_attacking(105501013)
+
+    skill_data = skill_data_base.with_conditions()
+
+    assert skill_data.hit_count == [4, 4]
+    assert skill_data.hit_count_at_max == 4
+    assert skill_data.total_mod == pytest.approx([
+        5.62 * 4,
+        6.22 * 4,
+    ])
+    assert skill_data.total_mod_at_max == pytest.approx(6.22 * 4)
+    assert skill_data.mods == approx_matrix([
+        [5.62] * 4,
+        [6.22] * 4,
+    ])
+    assert skill_data.mods_at_max == pytest.approx([6.22] * 4)
+    assert skill_data.max_level == 2
