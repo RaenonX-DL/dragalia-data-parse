2c351715e8fa3f4cc1ecb5805ca707930c258d9d
RaenonX <raenonx0710@gmail.com>
2021-08-04 04:05:21 -0500

    ADD - Translations for Summer Chelle skills
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- script_chara_overview.py
+++ script_chara_overview.py
@@ -149,7 +149,7 @@ def main():
     # - 10350505 (Joker)
     # - 10450404 (Sophie)
     # - 10550104 (Panther)
-    chara_skill_overview(10350103)
+    chara_skill_overview(10750404)
 
 
 if __name__ == '__main__':
